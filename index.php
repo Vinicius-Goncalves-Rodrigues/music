b72c79838b4b1b7ee08aa15b5c90f735b7f859b8
V1nny da0ra <viniciusrodrigues.sesisenai@gmail.com>
2024-11-13 22:52:34 -0300

    hh

--- index.php
+++ index.php
@@ -1,11 +1,58 @@
 <?php
-  $login_cookie = $_COOKIE["login"];
-    if(isset($login_cookie)){
-      echo"Bem-Vindo, $login_cookie <br>";
-      echo"Essas informações <font color="red">PODEM</font> ser acessadas por você";
-    }else{
-      echo"Bem-Vindo, convidado <br>";
-      echo"Essas informações <font color="red">NÃO PODEM</font> ser acessadas por você";
-      echo"<br><a href="login.html">Faça Login</a> Para ler o conteúdo";
-    }
+session_start();
+include_once 'controllers/UserController.php';
+
+$action = $_GET['action'] ?? '';
+
+$controller = new UserController();
+
+switch ($action) {
+    case 'register':
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $nome = $_POST['nome'];
+            $email = $_POST['email'];
+            $senha = $_POST['senha'];
+
+            if ($controller->register($nome, $email, $senha)) {
+                header("Location: index.php?action=login");
+            } else {
+                echo "Erro ao cadastrar usuário.";
+            }
+        } else {
+            include 'views/register.php';
+        }
+        break;
+
+    case 'login':
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $email = $_POST['email'];
+            $senha = $_POST['senha'];
+
+            if ($controller->login($email, $senha)) {
+                header("Location: index.php?action=home");
+            } else {
+                echo "Login ou senha inválidos.";
+            }
+        } else {
+            include 'views/login.php';
+        }
+        break;
+
+    case 'home':
+        if (isset($_SESSION['user_id'])) {
+            include 'views/home.php';
+        } else {
+            header("Location: index.php?action=login");
+        }
+        break;
+
+    case 'logout':
+        session_destroy();
+        header("Location: index.php?action=login");
+        break;
+
+    default:
+        header("Location: index.php?action=login");
+        break;
+}
 ?>
